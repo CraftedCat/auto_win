41a6a3587804b6f66e0ce24d653625ffba264ca5
Гаюнов Денис Валерьевич <denis.gayunov@sbermarket.ru>
2025-06-15 20:44:20 +0300

    12900k

--- get_12900k.sh
+++ get_12900k.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+INSTALL_FILE="install_12900k.sh"
 
-wget https://raw.githubusercontent.com/CraftedCat/auto_win/refs/heads/main/install_12900k.sh
+wget https://raw.githubusercontent.com/CraftedCat/auto_win/refs/heads/main/$INSTALL_FILE
 if [ -z "$1" ]; then
     echo "Использование: $0 <пароль для WebServer c файлами>"
     exit 1
@@ -8,12 +9,10 @@ fi
 
 PASSWORD_INSTALL="$1"
 
-
-# Замена {{password}} в install.sh
-if [ -f install_12900k.sh ]; then
-    sed -i "s|{{password}}|$PASSWORD_INSTALL|g" install_12900k.sh
+if [ -f $INSTALL_FILE ]; then
+    sed -i "s|{{password}}|$PASSWORD_INSTALL|g" $INSTALL_FILE
 else
     echo "Файл install_12900k.sh не найден!"
 fi
 
-bash install_12900k.sh
+bash $INSTALL_FILE
